5d7b1ac7fa92fa45b99767711723e2fd285fa5f6
gizemsangur1 <gizem2002sangur@gmail.com>
2025-07-19 13:18:57 +0300

    Add Movie model and Movie list/create API endpoint

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    poster_url = models.URLField()
+    duration_minutes = models.IntegerField()
+    release_date = models.DateField()
+
+    def __str__(self):
+        return self.title
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import generics
+from .models import Movie
+from .serializers import MovieSerializer
 
-# Create your views here.
+class MovieListCreateView(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
